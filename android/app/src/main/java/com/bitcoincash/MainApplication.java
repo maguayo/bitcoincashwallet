e35d3e0f9e9a7c192efa7421615578dbbaabff9f
Marcos Aguayo <marcos@aguayo.es>
2019-04-28 00:19:17 +0100

    Add fonts

--- android/app/src/main/java/com/bitcoincash/MainApplication.java
+++ android/app/src/main/java/com/bitcoincash/MainApplication.java
@@ -3,6 +3,7 @@ package com.bitcoincash;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNSensitiveInfoPackage(),
             new RNGestureHandlerPackage(),
             new RandomBytesPackage()
