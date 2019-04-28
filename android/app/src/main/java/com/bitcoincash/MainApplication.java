dfaee25bde3e29b6e4ab9a874cc7921dedaf8ec3
Marcos Aguayo <marcos@aguayo.es>
2019-04-28 18:45:00 +0100

    React Native Camera

--- android/app/src/main/java/com/bitcoincash/MainApplication.java
+++ android/app/src/main/java/com/bitcoincash/MainApplication.java
@@ -3,6 +3,7 @@ package com.bitcoincash;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new VectorIconsPackage(),
             new RNSensitiveInfoPackage(),
             new RNGestureHandlerPackage(),
--- screens/SendScreen.js
+++ screens/SendScreen.js
@@ -1,22 +1,57 @@
-import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import React, { Component } from 'react';
 
-export default class SendScreen extends React.Component {
-  static navigationOptions = {title: 'Send'};
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+export default class SendScreen extends Component {
+  onSuccess(e) {
+    Linking
+      .openURL(e.data)
+      .catch(err => console.error('An error occured', err));
+  }
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>Send here</Text>
-      </ScrollView>
+      <QRCodeScanner
+        onRead={this.onSuccess.bind(this)}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  centerText: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
   },
 });
